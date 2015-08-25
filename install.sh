0a3924a4057e42ea143d2daa862c94f8b5c7ab6a
Paul de Wouters <paul@hmn.md>
2015-08-25 10:57:34 +0200

    initial commit

---
+++ install.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+# Get current dir (so run this script from anywhere)
+
+export DOTFILES_DIR EXTRA_DIR
+DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+EXTRA_DIR="$HOME/.extra"
+
+# Update dotfiles itself first
+
+[ -d "$DOTFILES_DIR/.git" ] && git --work-tree="$DOTFILES_DIR" --git-dir="$DOTFILES_DIR/.git" pull origin master
+
+# Bunch of symlinks
+
+ln -sfv "$DOTFILES_DIR/runcom/.zshrc" ~
+# ln -sfv "$DOTFILES_DIR/runcom/.inputrc" ~
+ln -sfv "$DOTFILES_DIR/git/.gitconfig" ~
+ln -sfv "$DOTFILES_DIR/git/.gitignore_global" ~
+
+if [ "$(uname)" == "Darwin" ]; then
+    ln -sfv "$DOTFILES_DIR/etc/mackup/.mackup.cfg" ~
+    if [ -f "$DOTFILES_DIR/install/osx.sh" ]; then
+        . "$DOTFILES_DIR/install/osx.sh"
+    fi
+fi
+
+# Run tests
+
+bats test/*.bats
